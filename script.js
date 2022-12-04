46c3f3fca63d3ba6e2248bd3a0f53bd02ac1d2ce
MOHAMMAD SARFARAZ AFZAL <sanjum96786@gmail.com>
2022-12-04 13:52:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('😊😊😊😊😊😊😊😊😊', 'success')
+  })
+}
