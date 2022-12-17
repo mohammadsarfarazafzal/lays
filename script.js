55ae3a318d5550e1ca29bfd76896860c65ed19a8
MOHAMMAD SARFARAZ AFZAL <sanjum96786@gmail.com>
2022-12-17 18:37:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,107 @@ function hide(x) {
   x.style.border = "none";
   x.style.color = "transparent";
 }
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+    else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const observer1 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show1');
+    }
+    else {
+      entry.target.classList.remove('show1');
+    }
+  });
+});
+
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show2');
+    }
+    else {
+      entry.target.classList.remove('show2');
+    }
+  });
+});
+
+const observer3 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show3');
+    }
+    else {
+      entry.target.classList.remove('show3');
+    }
+  });
+});
+
+const observer4 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show4');
+    }
+    else {
+      entry.target.classList.remove('show4');
+    }
+  });
+});
+
+const observer5 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show5');
+    }
+    else {
+      entry.target.classList.remove('show5');
+    }
+  });
+});
+
+const observer6 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show6');
+    }
+    else {
+      entry.target.classList.remove('show6');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenElements1 = document.querySelectorAll('.hidden1');
+hiddenElements1.forEach((el) => observer1.observe(el));
+
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+hiddenElements2.forEach((el) => observer2.observe(el));
+
+const hiddenElements3 = document.querySelectorAll('.hidden3');
+hiddenElements3.forEach((el) => observer3.observe(el));
+
+const hiddenElements4 = document.querySelectorAll('.hidden4');
+hiddenElements4.forEach((el) => observer4.observe(el));
+
+const hiddenElements5 = document.querySelectorAll('.hidden5');
+hiddenElements5.forEach((el) => observer5.observe(el));
+
+const hiddenElements6 = document.querySelectorAll('.hidden6');
+hiddenElements6.forEach((el) => observer6.observe(el));
